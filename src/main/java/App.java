de0a1b907b142d63923cd283dccc6a39bf7b7aae
kangogopeter <peterkangogo501@gmail.com>
2019-05-26 16:38:56 +0300

    updated the .vtl file to link themselves

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,14 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+        get("/page", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("clients", Client.all());
+            model.put("template", "templates/clientspage.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
 
         get("/menu", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
