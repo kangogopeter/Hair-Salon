a06b41dd02b45ba9aabbf132050b9817012744ce
kangogopeter <peterkangogo501@gmail.com>
2019-05-28 12:37:25 +0300

    adding database

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,5 +1,6 @@
 import org.sql2o.*;
 
 public class DB{
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "peter", "1234");
+    String connectionString = "jdbc:postgresql://ec2-54-83-192-245.compute-1.amazonaws.com:5432/d7cloq5rodf6hg";
+    Sql2o sql2o = new Sql2o(connectionString, "xamoyjbwzusucq", "0392b2db5cc41e9bad74fa97c385daaa603725fbe932f4f01c3b369c0e7249c1");
 }
