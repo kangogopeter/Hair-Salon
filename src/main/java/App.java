d1a32514cdaadc9a15f58384c880453f6693bbad
kangogopeter <peterkangogo501@gmail.com>
2019-05-28 09:32:50 +0300

    addition of pox.xml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,15 @@ import spark.template.velocity.VelocityTemplateEngine;
 import java.util.Map;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         String layout = "templates/layout.vtl";
 
